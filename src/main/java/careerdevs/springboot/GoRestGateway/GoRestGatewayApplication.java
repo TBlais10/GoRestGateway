9e9c189add43645fad68f4562c877467bda4595e
TBlais10 <blaislighting@gmail.com>
2021-11-17 15:40:27 -0500

    Initial commit

---
+++ src/main/java/careerdevs/springboot/GoRestGateway/GoRestGatewayApplication.java
@@ -0,0 +1,13 @@
+package careerdevs.springboot.GoRestGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoRestGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoRestGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/careerdevs/springboot/GoRestGateway/GoRestGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package careerdevs.springboot.GoRestGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoRestGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
